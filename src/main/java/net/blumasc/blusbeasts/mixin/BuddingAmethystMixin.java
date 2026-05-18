e08e592911a012e7e51cea0dd2741602c18e73bc
blumasc <blumasc@googlemail.com>
2026-05-18 06:43:12 +0200

    fixed mixin using client directions class

--- src/main/java/net/blumasc/blusbeasts/mixin/BuddingAmethystMixin.java
+++ src/main/java/net/blumasc/blusbeasts/mixin/BuddingAmethystMixin.java
@@ -14,7 +14,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static net.minecraft.client.renderer.LevelRenderer.DIRECTIONS;
 import static net.minecraft.world.level.block.BuddingAmethystBlock.canClusterGrowAtState;
 
 @Mixin(BuddingAmethystBlock.class)
@@ -29,7 +28,7 @@ public class BuddingAmethystMixin {
     protected void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random, CallbackInfo ci) {
         if(!Config.SPAWN_AMETHYST_CRAB.get()) return;
         if (random.nextInt(30) == 0) {
-            Direction direction = DIRECTIONS[random.nextInt(DIRECTIONS.length)];
+            Direction direction = Direction.values()[random.nextInt(Direction.values().length)];
             BlockPos blockpos = pos.relative(direction);
             BlockState blockstate = level.getBlockState(blockpos);
             if (canClusterGrowAtState(blockstate)) {
